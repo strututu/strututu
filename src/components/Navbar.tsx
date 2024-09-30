fc33bd9ea74b09d9bdac9a3d84a9213bf5d520e1
strututu <hadid29032006@gmail.com>
2024-09-30 09:19:09 +0700

    fix nav url

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ const Navbar = () => {
   return (
     <nav className="w-full px-10 h-[100px] flex items-center">
       <a
-        href="/"
+        href="./"
         className="text-[#FFEBD0] text-[42px] font-medium font-serif hover:text-[#e7c9a4] transition-all duration-300 max-[1000px]:text-[24px]"
       >
         strututu.
